15952832100fb29ae34610d05fc60f9669e446e4
victorandradeap <victorandradeap@gmail.com>
2021-09-23 13:05:34 -0300

    Create entrypoint

---
+++ bin/index.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+const sqs = require("./sqs");
+const lambda = require("./lambda");
+const options = require('./args');
+const chalk = require("chalk");
+
+console.log('\n');
+execute();
+
+async function execute() {
+    const messages = await sqs.getMessages(options.queueUrl, options.maxReceiveCount);
+
+    let responses = [];
+    for (let message of messages) {
+        let response = await lambda.invoke(options.functionName, message);
+        responses.push(response);
+    }
+
+    for (let response of responses) {
+        if (!response.error) {
+            await sqs.deleteMessages(options.queueUrl, response.messageId, response.messageReceiptHandle);
+        }
+    }
+
+    const successItems = responses.filter(response => !response.error);
+    if(successItems > 0) {
+        console.info(chalk.green(`\n${successItems.length} messages processed successfully.`));
+    }
+
+    const errorsItems = responses.filter(response => response.error);
+    if(errorsItems > 0) {
+        console.info(chalk.red(`\n${errorsItems.length} messages processed with error.`));
+    }
+}
